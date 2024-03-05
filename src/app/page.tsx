e94e2f06aa413d2f1fcf760f1292d0147cb6aec3
devbylanre <devbylanre@gmail.com>
2024-03-05 11:28:27 +0100

    layout and pag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image';
+import { Container } from '@/components/Container';
+import Feature from '@/sections/Feature';
+import Hero from '@/sections/Hero';
+import Service from '@/sections/Service';
+import Skill from '@/sections/Skill';
+import Work from '@/sections/Work';
+
+export const metadata = {
+  title: 'Ola | Website designer',
+};
 
 export default function Home() {
   return (
-    <div className=''>
-      <h1>hello world</h1>
-    </div>
+    <Container as={'main'}>
+      {/* sections */}
+      <Hero />
+      <Skill />
+      <Service />
+      <Feature />
+      <Work />
+    </Container>
   );
 }
