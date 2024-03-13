6841fd0baab7cbdeaae8f31bb2d8ecec1c0fef12
devbylanre <devbylanre@gmail.com>
2024-03-13 11:09:59 +0100

    added accordion component context

--- src/contexts/components/Accordion.tsx
+++ src/contexts/components/Accordion.tsx
@@ -24,7 +24,7 @@ export const AccordionProvider = ({
   children,
   opened,
 }: AccordionProviderProps) => {
-  const [accordions, setAccordions] = useState<string[]>([''] || [opened]);
+  const [accordions, setAccordions] = useState<string[]>([opened || '']);
 
   return (
     <accordionContext.Provider
